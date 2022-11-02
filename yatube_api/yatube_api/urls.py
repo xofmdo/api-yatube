8b241c1413ad0edc203e05c1f837bceedb4aa861
Alexandr Kuzmich <92593750+xofmdo@users.noreply.github.com>
2022-11-02 14:39:02 +0300

    made authorization by jwt and created empty files

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -2,9 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
 
 
